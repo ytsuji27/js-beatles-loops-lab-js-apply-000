35130a193fa442cc17fd78bc470aeb270f3f74a1
Yutaro Tsuji <ytsuji91@gmail.com>
2018-12-26 22:53:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
+  var i;
+  for(i = 0; i < musicians.length; i++) {
+    var string = `${musicians[i]} plays ${instruments[i]}`
+    arr.push(string)
+  }
+  return arr
+}
+
+
+function johnLennonFacts(facts) {
+  var arr = [];
+  var i = 0;
+  while(i < facts.length) {
+    arr.push(`${facts[i]}!!!`);
+    i++;
+    }
+    return arr
+  }
